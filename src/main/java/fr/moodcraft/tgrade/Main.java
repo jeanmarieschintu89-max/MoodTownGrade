a1ad13d87dd854d49d1b6289cedf9b4849cd35c4
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-06 19:12:05 +0200

    Create src/main/java/fr/moodcraft/tgrade/Main.java

---
+++ src/main/java/fr/moodcraft/tgrade/Main.java
@@ -0,0 +1,68 @@
+package fr.moodcraft.tgrade;
+
+import fr.moodcraft.tgrade.command.VilleCommand;
+import fr.moodcraft.tgrade.storage.SubmissionStorage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    //
+    // 🌍 INSTANCE
+    //
+
+    private static Main instance;
+
+    public static Main get() {
+        return instance;
+    }
+
+    //
+    // 🚀 ENABLE
+    //
+
+    @Override
+    public void onEnable() {
+
+        //
+        // 📌 INSTANCE
+        //
+
+        instance = this;
+
+        //
+        // 📂 CONFIG
+        //
+
+        saveDefaultConfig();
+
+        //
+        // 💾 STORAGE
+        //
+
+        SubmissionStorage.init();
+
+        //
+        // 📜 COMMANDES
+        //
+
+        getCommand("ville")
+                .setExecutor(new VilleCommand());
+
+        //
+        // ✅ CONSOLE
+        //
+
+        getLogger().info("MoodTownGrade chargé ✔");
+    }
+
+    //
+    // 🔻 DISABLE
+    //
+
+    @Override
+    public void onDisable() {
+
+        getLogger().info("MoodTownGrade arrêté ✖");
+    }
+}
