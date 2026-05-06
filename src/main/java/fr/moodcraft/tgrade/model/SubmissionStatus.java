76b218d5883d24575c828fcee35167c74e518f90
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-06 19:13:09 +0200

    Create src/main/java/fr/moodcraft/tgrade/model/SubmissionStatus.java

---
+++ src/main/java/fr/moodcraft/tgrade/model/SubmissionStatus.java
@@ -0,0 +1,22 @@
+package fr.moodcraft.tgrade.model;
+
+public enum SubmissionStatus {
+
+    //
+    // 🟡 EN ATTENTE
+    //
+
+    PENDING,
+
+    //
+    // 🟢 VALIDÉ
+    //
+
+    APPROVED,
+
+    //
+    // 🔴 REFUSÉ
+    //
+
+    REJECTED
+}
