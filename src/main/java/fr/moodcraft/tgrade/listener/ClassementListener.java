380c4127c5f0bd483b9d70cbd5f38ceb1ec1e0e5
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-07 17:46:55 +0200

    Create ClassementListener.java

---
+++ src/main/java/fr/moodcraft/tgrade/listener/ClassementListener.java
@@ -0,0 +1,94 @@
+package fr.moodcraft.tgrade.listener;
+
+import fr.moodcraft.tgrade.gui.ClassementGUI;
+import fr.moodcraft.tgrade.gui.UrbanismeMainGUI;
+
+import org.bukkit.Sound;
+
+import org.bukkit.entity.Player;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class ClassementListener
+        implements Listener {
+
+    //
+    // 🏆 CLICK
+    //
+
+    @EventHandler
+    public void click(
+            InventoryClickEvent e
+    ) {
+
+        //
+        // 📛 TITLE
+        //
+
+        if (!e.getView()
+                .getTitle()
+                .equalsIgnoreCase(
+                        "§8🏆 Classement National"
+                )) {
+            return;
+        }
+
+        //
+        // ❌ CANCEL
+        //
+
+        e.setCancelled(true);
+
+        //
+        // 👤 PLAYER
+        //
+
+        if (!(e.getWhoClicked()
+                instanceof Player p))
+            return;
+
+        //
+        // 📦 INVENTORY CHECK
+        //
+
+        if (e.getClickedInventory() == null)
+            return;
+
+        //
+        // 📦 ITEM
+        //
+
+        if (e.getCurrentItem() == null)
+            return;
+
+        //
+        // 🔘 SLOT
+        //
+
+        int slot =
+                e.getRawSlot();
+
+        //
+        // 🔙 RETOUR
+        //
+
+        if (slot == 49) {
+
+            p.playSound(
+
+                    p.getLocation(),
+
+                    Sound.UI_BUTTON_CLICK,
+
+                    1f,
+
+                    1f
+            );
+
+            UrbanismeMainGUI.open(p);
+        }
+    }
+}
