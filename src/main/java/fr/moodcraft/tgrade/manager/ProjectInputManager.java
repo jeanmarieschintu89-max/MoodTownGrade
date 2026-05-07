14c181b1f3941f084b6e362f06ea863f234149a0
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-07 18:54:53 +0200

    Create ProjectInputManager.java

---
+++ src/main/java/fr/moodcraft/tgrade/manager/ProjectInputManager.java
@@ -0,0 +1,43 @@
+package fr.moodcraft.tgrade.manager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.UUID;
+
+public class ProjectInputManager {
+
+    //
+    // 🧠 INPUT WAITING
+    //
+
+    private static final Map<UUID, Boolean>
+            waiting = new HashMap<>();
+
+    //
+    // ➕ START
+    //
+
+    public static void start(UUID uuid) {
+
+        waiting.put(uuid, true);
+    }
+
+    //
+    // ❌ STOP
+    //
+
+    public static void stop(UUID uuid) {
+
+        waiting.remove(uuid);
+    }
+
+    //
+    // 🔍 CHECK
+    //
+
+    public static boolean isWaiting(UUID uuid) {
+
+        return waiting.containsKey(uuid);
+    }
+}
